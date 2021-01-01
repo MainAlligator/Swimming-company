24d51801d6ae4b5b21e3e73ce4f1e77f0ef70768
Alexi Krol <krol.mats@gmail.com>
2021-01-01 21:01:21 +0200

    First commit

---
+++ src/com/swimming/Cuboid.java
@@ -0,0 +1,25 @@
+package com.swimming;
+
+class Cuboid extends Rectangle {
+
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height;
+
+    }
+
+    public double getHeight() {
+        if (height < 0) {
+            this.height = 0;
+        }
+        return height;
+    }
+
+    public double getVolume() {
+
+        return height * getArea();
+    }
+}
+
